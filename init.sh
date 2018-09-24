4b8e765d275b1a0349489be79787a24d880a8436
Evgeny_Rudenko <evgeniy.rudenko@gost-group.com>
2018-09-24 10:23:25 +0300

    еще одна правка конфигов

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 
 sudo /etc/init.d/gunicorn restart
 sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
-sudo gunicorn -c /home/box/web/etc/django_conf.py ask.wsgi:application 
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application         
+sudo gunicorn -c /home/box/web/etc/django_gunicorn.conf ask.wsgi:application
