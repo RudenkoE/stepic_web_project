393d21ed024f87fde9a6010fdbe0c9aaf647728a
Evgeny_Rudenko <evgeniy.rudenko@gost-group.com>
2018-09-24 11:13:07 +0300

    Work version

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,27 +1,27 @@
 """ask URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls import url, include, patterns
 
-urlpatterns = patterns('qa.views'urlpatterns = patterns('qa.views',
-   url(r'^$', 'test'),
-   url(r'^login/.*$', 'test', name='login'),
-   url(r'^signup/.*', 'test', name='signup'),
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
-   url(r'^ask/.*', 'test', name='ask'),
-   url(r'^popular/.*', 'test', name='popular'),
-   url(r'^new/.*', 'test', name='new'),
-)
+urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+    url(r'^$', include('qa.urls')),
+    url(r'^login/$', include('qa.urls')),
+    url(r'^signup/$', include('qa.urls')),
+    url(r'^question/\d+/$', include('qa.urls')),
+    url(r'^ask/$', include('qa.urls')),
+    url(r'^popular/$', include('qa.urls')),
+    url(r'^new/$', include('qa.urls')),
+]
--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# Nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
+gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
