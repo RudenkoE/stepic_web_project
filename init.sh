cab3e4970859c043ddaa96165f27bbe3a9a9763a
Evgeny_Rudenko <evgeniy.rudenko@gost-group.com>
2018-09-26 12:52:14 +0300

    change init 4

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorndjango.conf /etc/gunicorn.d/testdj
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE qabase;"
-mysql -uroot -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY '123456';"
-mysql -uroot -e "GRANT ALL ON qabase.* TO 'qauser'@'localhost';"
+mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
+mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
