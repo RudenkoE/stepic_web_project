ffed55e9d0a01828292fc1f797f1dc002b127fb7
Evgeny_Rudenko <evgeniy.rudenko@gost-group.com>
2018-09-26 12:30:26 +0300

    Change init 1

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
-# Nginx
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-# Gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
-gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorndjango.conf /etc/gunicorn.d/testdj
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE qabase;"
+mysql -uroot -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY '123456';"
+mysql -uroot -e "GRANT ALL ON qabase.* TO 'qauser'@'localhost';"
