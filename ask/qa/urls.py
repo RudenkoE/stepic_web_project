bdfceed1ae84037d890ed6fdfff3397e2fc8ef89
Evgeny_Rudenko <evgeniy.rudenko@gost-group.com>
2018-09-26 15:17:16 +0300

    OD 7

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls import url
-from qa.views import test, mainpg, question, popular, question_ask, question_ans, user_login, user_signup, user_logout
+from qa.views import question
 
 urlpatterns = [
-	url(r'^$', mainpg, name='mainpg'),
-	url(r'^question/(?P<pk>\d+)/$', question, name='question'),
-	url(r'^popular/', popular, name='popular'),
-	url(r'^ask/', question_ask, name='question_ask'),
-	url(r'^answer/', question_ans, name='question_ans'),
-	url(r'^new/', test, name='test'),
-	url(r'^login/', user_login, name='login'),
-	url(r'^signup/', user_signup, name='signup'),
-	url(r'^logout/', user_logout, name='logout'),
+    url(r'^(?P<num>\d+)/$', question),
 ]
