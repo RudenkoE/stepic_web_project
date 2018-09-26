ada24618980b9107ff56c95522e9940e9ef816fc
Evgeny_Rudenko <evgeniy.rudenko@gost-group.com>
2018-09-26 16:15:16 +0300

    OD 11

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,23 +1,9 @@
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import include, url
 
-from ask.views import found, not_found, init25
-from qa.views import index, popular, ask, login_view, signup
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-
-    url(r'^$', index),
-
-    url(r'^init25/', init25),
-    url(r'^login/', login_view),
-    url(r'^signup/', signup),
-    url(r'^ask/', ask),
-    # url(r'^answer/', answer),
-    url(r'^popular/', popular),
-    url(r'^new/', found),
-
+    url(r'^', include('qa.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^question/', include('qa.urls')),
-
-    url(r'^', not_found),
 ]
